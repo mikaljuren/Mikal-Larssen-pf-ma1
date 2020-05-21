09fdc83e9aa084d90e12eff4a830a6614b8e9800
mikaljuren <63348364+mikaljuren@users.noreply.github.com>
2020-05-21 14:46:51 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+// Question 1
+
+var firstVar;
+
+// Question 2
+var name;
+var name = "Mikal";
+console.log(name);
+
+//Question 3
+var modulenumber;
+var modulenumber = "100";
+console.log(modulenumber);
+
+//Question 4
+var division;
+var division = 20 / 4;
+
+//Question 5
+var myString;
+var myString = "frog";
+
+// is myString equal to "frog">
+
+(myString === "frog") //true
+
+// Question 6 & 7
+var orderHasShipped;
+var orderHasShipped = true;
+
+if(orderHasShipped === true) {
+    console.log("The order has shipped");
+}
+
+else {
+    console.log("The order has not been shipped");
+}
+
+// Question 8
+for(var count = 0; count <= 9; count++) {
+    console.log(count);
+}
+
+
